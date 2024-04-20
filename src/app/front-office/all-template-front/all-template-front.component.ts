69c3203b5f2305b60a62831f47825b959c200341
SnapeV1 <Hamada_amine@yahoo.com>
2024-04-21 00:16:26 +0100

    profile picture

--- src/app/front-office/all-template-front/all-template-front.component.ts
+++ src/app/front-office/all-template-front/all-template-front.component.ts
@@ -1,10 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { UserModule } from 'src/app/Models/user.module';
+import { AuthService } from 'src/app/Service/auth.service';
 
 @Component({
   selector: 'app-all-template-front',
   templateUrl: './all-template-front.component.html',
   styleUrls: ['./all-template-front.component.css']
 })
-export class AllTemplateFrontComponent {
 
+export class AllTemplateFrontComponent {
+  userProfile: UserModule;
+
+  constructor(private authService: AuthService ,private http: HttpClient) {
+
+    this.userProfile = new UserModule(); 
+  }
+
+  ngOnInit(): void {
+    this.getUserInfoFromToken();
+  }
+
+
+
+
+
+
+
+
+
+  getUserInfoFromToken(): void {
+    this.authService.getUserInfoFromToken().subscribe(
+      (response: UserModule) => {
+        this.userProfile = response;
+      },
+      error => {
+        console.error('Error fetching user information:', error);
+      }
+    );
+  }
 }
--- src/app/front-office/navbar/navbar.component.ts
+++ src/app/front-office/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserModule } from 'src/app/Models/user.module';
 import { AuthService } from 'src/app/Service/auth.service';
 
 @Component({
@@ -8,9 +9,41 @@ import { AuthService } from 'src/app/Service/auth.service';
 })
 export class NavbarComponent {
   constructor(private authService: AuthService) { }
-
+  userProfile: UserModule;
   signOut(): void {
     this.authService.logoutAndRedirect();
+    this.userProfile = new UserModule(); 
+    console.log("aaaaaaaaa" +this.userProfile.picture)
   }
+
+ 
+
+
+  
+  
+
+  ngOnInit(): void {
+    this.getUserInfoFromToken();
+  }
+
+
+
+
+
+
+
+
+
+  getUserInfoFromToken(): void {
+    this.authService.getUserInfoFromToken().subscribe(
+      (response: UserModule) => {
+        this.userProfile = response;
+      },
+      error => {
+        console.error('Error fetching user information:', error);
+      }
+    );
+  }
+
 
 }
