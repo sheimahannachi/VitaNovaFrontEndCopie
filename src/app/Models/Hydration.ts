06614b764ffaed55d5c3e154acc6fa7256cc9450
onsghmougui <114781952+onsghmougui@users.noreply.github.com>
2024-04-26 18:30:24 +0100

    Create Hydration.ts

---
+++ src/app/Models/Hydration.ts
@@ -0,0 +1,12 @@
+import {Tracker} from "./Tracker";
+
+
+export class Hydration {
+  id!:number;
+  cupsQty!: number;
+  sumOfWater!: number;
+  notification!: string;
+  date!:Date;
+  archive!: boolean;
+  tracker!: Tracker;
+}
