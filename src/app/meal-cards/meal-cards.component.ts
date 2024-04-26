79836eac51dddc650a4946accbdb06e816ce82f7
onsghmougui <114781952+onsghmougui@users.noreply.github.com>
2024-04-26 18:37:36 +0100

    Add files via upload

---
+++ src/app/meal-cards/meal-cards.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../Service/food.service';
+import { Router } from '@angular/router';
+import { MealType } from '../Models/MealType';
+import { FoodCard } from '../Models/FoodCard';
+import {Tracker} from "../Models/Tracker";
+
+@Component({
+  selector: 'app-meal-cards',
+  templateUrl: './meal-cards.component.html',
+  styleUrls: ['./meal-cards.component.css']
+})
+export class MealCardsComponent implements OnInit {
+  breakfastFoodCards: FoodCard[] = [];
+  lunchFoodCards: FoodCard[] = [];
+  dinnerFoodCards: FoodCard[] = [];
+  snacksFoodCards: FoodCard[] = [];
+  mealtype: MealType;
+  idtracker: number;
+
+  constructor(private foodService: FoodService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+
+  addFoodToMeal(mealType: MealType): void {
+    // Navigate to the food front component with the meal type as a parameter
+    this.router.navigate(['vitaNova/foodFront'], {queryParams: {mealType: mealType}});
+  }
+
+  protected readonly MealType = MealType;
+}
